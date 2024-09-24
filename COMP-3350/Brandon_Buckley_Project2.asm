60b098b30f5d4d85332d2a7aef2c98072af2dac1
Brandon Buckley <brandonmb0136@gmail.com>
2024-09-24 09:03:36 -0500

    begin project 2

--- COMP-3350/Brandon_Buckley_Project2.asm
+++ COMP-3350/Brandon_Buckley_Project2.asm
@@ -2,8 +2,18 @@
 .model flat, stdcall
 .stack 4096
 ExitProcess PROTO, dwExitCode: DWORD
+.data
+input BYTE 1, 2, 3, 4, 5, 6, 7, 8
+shift BYTE 2
 .code
 main PROC
+	mov eax, 0
+	mov ebx, 0
+	mov ecx, 0
+	mov edx, 0
+
+
+
 	INVOKE ExitProcess, 0
 main ENDP
 END main
