8ec995f961bb6c578acbc2d696eab8d48de5abef
Brandon Buckley <brandonmb0136@gmail.com>
2024-10-02 14:46:01 -0500

    do project 2

--- Project2/Brandon_Buckley_Project2.asm
+++ Project2/Brandon_Buckley_Project2.asm
@@ -7,12 +7,34 @@ input BYTE 1, 2, 3, 4, 5, 6, 7, 8
 shift BYTE 2
 .code
 main PROC
-	mov eax, 0
-	mov ebx, 0
-	mov ecx, 0
-	mov edx, 0
+	mov EAX, 0
+	mov EBX, 0
+	mov ECX, 0
+	mov EDX, 0
 
+	mov AH, input[0]
+	add AH, shift
 
+	mov AL, input[1]
+	add AL, shift
+
+	mov BH, input[2]
+	add BH, shift
+
+	mov BL, input[3]
+	add BL, shift
+
+	mov CH, input[4]
+	add CH, shift
+
+	mov CL, input[5]
+	add CL, shift
+
+	mov DH, input[6]
+	add DH, shift
+
+	mov DL, input[7]
+	add DL, shift
 
 	INVOKE ExitProcess, 0
 main ENDP
