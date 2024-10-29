9a67df3d78a2bbb089f5df14c35537d6b7b67f95
Brandon Buckley <brandonmb0136@gmail.com>
2024-10-29 09:15:24 -0500

    Setup project 4

---
+++ Project4/Brandon_Buckley_project4.asm
@@ -0,0 +1,16 @@
+; Brandon Buckley
+; Brandon_Buckley_Project4.asm
+;
+; TODO: help used
+
+
+.386
+.model flat, stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode: DWORD
+.data
+.code
+main PROC
+main ENDP
+
+end main
