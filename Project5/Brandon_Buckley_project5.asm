097d330e9ca9747f9babbeb126a732dd461d59ad
Brandon Buckley <brandonmb0136@gmail.com>
2024-11-11 14:02:17 -0600

    Add GetKeyByte

--- Project5/Brandon_Buckley_project5.asm
+++ Project5/Brandon_Buckley_project5.asm
@@ -17,4 +17,27 @@ output	BYTE	0 DUP(LENGTHOF input)
 main PROC
 	INVOKE ExitProcess, 0
 main ENDP
+
+; GetKeyByte(i)
+; Arguments: eax = i
+; Returns: eax = key[i % len(key)] - 'A'
+; Preconditions: len(key) > 0, eax >= 0
+GetKeyByte PROC
+	push edx
+	push ecx
+
+	mov edx, 0
+	mov ecx, LENGTHOF key
+	div ecx
+	mov eax, edx
+
+	pop ecx
+	pop edx
+
+	movzx eax, [key + eax]
+	sub eax, 'A'
+
+	ret
+GetKeyByte ENDP
+
 END main
